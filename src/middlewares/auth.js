843383750852cd405255819dce5f3d2e88fba367
Trisumanzaya93 <trisumanzaya93a@gmail.com>
2022-02-22 01:58:14 +0700

    bug token

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -6,7 +6,7 @@ const response = require("../helper/response");
 const checkToken = async(req, res, next) => {
   const token = req.header('x-access-token')
   console.log('tokenfahrul',token)
-  if (token===undefined) { return response(res, { status: 403, message: "gara gara fahrul" }); }
+  if (token===undefined) { return response(res, { status: 403, message: "token undefined" }); }
   const checkWhiteList= await model.white_list_tokens.findOne({where:{token}})
   if (checkWhiteList) { return response(res, { status: 403, message: "you already logout" }); }
   // const sqlGetBlackList = `SELECT token FROM white_list_token WHERE token = ?`
